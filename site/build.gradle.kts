780b6f0e9fef58452204dccfd6330337d8f9463f
Stevdza-San <stefan.jovanavich@gmail.com>
2024-01-01 15:31:58 +0100

    Initial commit

---
+++ site/build.gradle.kts
@@ -0,0 +1,41 @@
+import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kobweb.application)
+    // alias(libs.plugins.kobwebx.markdown)
+}
+
+group = "com.stevdza_san.website"
+version = "1.0-SNAPSHOT"
+
+kobweb {
+    app {
+        index {
+            description.set("Powered by Kobweb")
+        }
+    }
+}
+
+kotlin {
+    configAsKobwebApplication("website")
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.html.core)
+                implementation(libs.kobweb.core)
+                implementation(libs.kobweb.silk)
+                implementation(libs.silk.icons.fa)
+                // implementation(libs.kobwebx.markdown)
+            }
+        }
+    }
+}
